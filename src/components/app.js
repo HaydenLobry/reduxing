ad041108d0573d8f37247f80bf8aa50fe0a5a25c
Hayden Lobry <Hayden.Lobry@iag.com.au>
2018-09-05 14:30:31 +1000

    Set-up and styling for solo project

--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <h3 className="heading" >Get the weather outlook for major cities around the world!</h3>
         <SearchBar />
         <CityList />
       </div>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+// const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOOLS_EXTENSION__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(ReduxPromise));
+const store = createStore(reducers, enhancer);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
   , document.querySelector('.container'));
